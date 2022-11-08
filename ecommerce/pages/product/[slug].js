6b356ee401e3ad9376afe535a0a70b48ca8b8d85
Elkennard <100434303+Elkennard@users.noreply.github.com>
2022-11-08 16:10:09 +0000

    adimplemented the may also like tracker and small images for items

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AiOutlineMinus,
   AiOutlinePlus,
@@ -7,29 +7,31 @@ import {
 } from "react-icons/ai";
 
 import { client, urlFor } from "../../lib/client";
+import { Product } from "../../components";
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
+  const [index, setIndex] = useState(0);
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[0])} />
+            <img src={urlFor(image && image[index])} className="product-detail-image" />
           </div>
-          {/* <div className="small-images-container">
+          <div className="small-images-container">
             {image?.map((item, i) => (
                 <img
                 src={urlFor(item)}
-                className=""
-                onMouseEnter=""
+                className={i === index ? 'small-image selected-image' : 'small-image'}
+                onMouseEnter={() => setIndex(i)}
                 />
 
             ))}
-          </div> */}
+          </div>
         </div>
 
-        <div className="product-details-desc">
+        <div className="product-detail-desc">
           <h1>{name}</h1>
           <div className="reviews">
             <div>
@@ -44,7 +46,7 @@ const ProductDetails = ({ product, products }) => {
           <h4>Details: </h4>
           <p>{details}</p>
           <p className="price">£{price}</p>
-          <div className="qunatity">
+          <div className="quantity">
             <h3>Quantity:</h3>
             <p className="quantity-desc">
             <span className="minus"
@@ -60,6 +62,16 @@ const ProductDetails = ({ product, products }) => {
 <button type="button" className="add-to-cart" onClick="">Add to Cart</button>
 <button type="button" className="buy-now" onClick="">Buy Now</button>
           </div>
+        </div>
+      </div>
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+<div className="maylike-products-container track">
+    {products.map((item) => (
+        <Product key={item._id} product={item} />
+    ))}
+</div>
         </div>
       </div>
     </div>
